cda8d5f0824f888ee8cb22712f66daf7a6568a0d
Henry Scheible <henry.scheible@gmail.com>
2024-02-25 13:45:13 -0500

    update parameters.py for experiment with regularization

--- parameters.py
+++ parameters.py
@@ -1,16 +1,56 @@
-arg_options = {
-    "datadir": ["datasets"],
-    "nchannels": [3],
-    "nclasses": [10],
-    "nunits": [500, 1000, 2000],
-    "lr": [0.01],
-    "mt": [0.9],
-    "batchsize": [256],
-    "epochs": [40],
-    "stopcond": [0.01], 
-    "l1": [0.0],
-    "l2": [0.0],
-    "dropout": [0.0],
-    "nlayers": [3],
-    "rank_constraint": [0,10,20,40,60,80,500,1000,2000]
-}
+# Regularization experiments
+
+arg_options = [
+    {
+        "datadir": ["datasets"],
+        "nchannels": [3],
+        "nclasses": [10],
+        "nunits": [256],
+        "lr": [0.01],
+        "mt": [0.9],
+        "batchsize": [256],
+        "epochs": [40],
+        "stopcond": [0.01], 
+        "l1": [0.0],
+        "l2": [0.0],
+        "dropout": [0.0],
+        "nlayers": [3],
+        "rank_constraint": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220]
+    }
+]
+
+
+# arg_options = [
+#     {
+#         "datadir": ["datasets"],
+#         "nchannels": [3],
+#         "nclasses": [10],
+#         "nunits": [256],
+#         "lr": [0.01],
+#         "mt": [0.9],
+#         "batchsize": [256],
+#         "epochs": [40],
+#         "stopcond": [0.01], 
+#         "l1": [0.0],
+#         "l2": [0.0],
+#         "dropout": [0.0],
+#         "nlayers": [3],
+#         "rank_constraint": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220]
+#     },
+#     {
+#         "datadir": ["datasets"],
+#         "nchannels": [3],
+#         "nclasses": [10],
+#         "nunits": [1024],
+#         "lr": [0.01],
+#         "mt": [0.9],
+#         "batchsize": [256],
+#         "epochs": [40],
+#         "stopcond": [0.01], 
+#         "l1": [0.0],
+#         "l2": [0.0],
+#         "dropout": [0.0],
+#         "nlayers": [3],
+#         "rank_constraint": [1,2,3,4,5,6,7,8,9,10,11,12,13,20,30,40,50,75,100,150, 200, 220, 250, 300, 350, 400, 500, 600, 700, 800, 900, 1000]
+#     },
+# ]
