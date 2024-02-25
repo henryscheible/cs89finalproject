699c1a4e8b4a455b0970d8279ba30cd03a27ac1a
Henry Scheible <henry.scheible@gmail.com>
2024-02-25 14:22:13 -0500

    improve num parameters calculator

--- compute_num_parameters.py
+++ compute_num_parameters.py
@@ -16,16 +16,18 @@ if __name__ == '__main__':
     random.seed(0)
 
     model_file = torch.load(args.checkpoint_path)
-    nchannels = model_file['args']['nchannels']
-    nunits = model_file['args']['nunits']
-    nclasses = model_file['args']['nclasses']
-    nlayers = model_file['args']['nlayers']
-    rank_constraint = model_file["args"]["rank_constraint"]
-    print(f"nchannels: {nchannels}, nunits: {nunits}, nclasses: {nclasses}, nlayers: {nlayers}, rank_constraint: {rank_constraint}")
+    # nchannels = model_file['args']['nchannels']
+    # nunits = model_file['args']['nunits']
+    # nclasses = model_file['args']['nclasses']
+    # nlayers = model_file['args']['nlayers']
+    # rank_constraint = model_file["args"]["rank_constraint"]
+    # print(f"nchannels: {nchannels}, nunits: {nunits}, nclasses: {nclasses}, nlayers: {nlayers}, rank_constraint: {rank_constraint}")
 
-    if model_file["args"]["rank_constraint"] == 0:
-        model = make_model(nchannels=nchannels, nunits=nunits, nclasses=nclasses, nlayers=nlayers)
-    else:
-        model = make_rank_k_model(nchannels=nchannels, nunits=nunits, nclasses=nclasses, nlayers=nlayers, k=model_file["args"]["rank_constraint"])
+    # if model_file["args"]["rank_constraint"] == 0:
+    #     model = make_model(nchannels=nchannels, nunits=nunits, nclasses=nclasses, nlayers=nlayers)
+    # else:
+    #     model = make_rank_k_model(nchannels=nchannels, nunits=nunits, nclasses=nclasses, nlayers=nlayers, k=model_file["args"]["rank_constraint"])
 
-    print(f"Total Parameters: {sum([np.prod(p.size()) for p in model.parameters()])}")
+    if "model" in model_file.keys():
+        model_file = model_file["model"]
+    print(f"Total Parameters: {sum([p.numel() for p in model_file.values()])}")
