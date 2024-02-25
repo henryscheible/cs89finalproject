cab8a1e326475465836bd4438cb6d16e14d99678
Jeffrey Jiang <jeffj@Jeffreys-MacBook-Air.local>
2024-02-25 13:46:05 -0500

    more figures

--- linalg.py
+++ linalg.py
@@ -11,19 +11,20 @@ def show_spec_density(weight_layer_name, weight_matrix, fig, axs, i=0,):
     # Plot the singular values
 
     axs[i].hist(singular_values)
-    axs[i].set_title(f'Empirical Spectral Density for layer {weight_layer_name}', fontsize=8)
+    axs[i].set_title(f'ESD for {weight_layer_name}', fontsize=9)
     axs[i].set_xlabel('Singular Value')
     axs[i].set_ylabel('Freq.')
     # plt.show()
 
 def show_all_spec_density(weight_dict):
-    fig, axs = plt.subplots(1, int(len(weight_dict)/2), figsize=(30, 8))
+    fig, axs = plt.subplots(1, int(len(weight_dict)/2), figsize=(20, 8))
     weight_idx=0
     for k, v in weight_dict.items():
         if 'weight' not in k:
             continue
         show_spec_density(k, v, fig, axs, weight_idx)
         weight_idx+=1
+    plt.subplots_adjust(wspace=.5)
     plt.show()
 
 
@@ -35,4 +36,8 @@ if __name__ == "__main__":
     k = 128
     convert_weight_to_low_rank(weight_dict, k)
 
-    show_all_spec_density(weight_dict)
+
+    weights_rank_train = torch.load('models/nlayers=3_k=128.pt',  map_location=torch.device('cpu'))['model state']
+    # print(weights_rank_train)
+    
+    show_all_spec_density(weights_rank_train)
